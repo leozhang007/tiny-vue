e3139080723fc0a2f495e5e91bc6276970cbbb06
LeoZhang <117000082@qq.com>
2020-07-22 18:34:26 +0800

    add:

--- tiny-router/src/router/index.js
+++ tiny-router/src/router/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import VueRouter from '../vuerouter'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -16,12 +16,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
--- tiny-router/src/vuerouter/index.js
+++ tiny-router/src/vuerouter/index.js
@@ -1,90 +1,95 @@
-let _Vue = null;
+let _Vue = null
 export default class VueRouter {
-  static install(Vue) {
+  static install (Vue) {
     // 1. 判断当前插件是否已经被安装
     if (VueRouter.install.installed) {
-      return;
+      return
     }
 
-    VueRouter.install.installed = true;
+    VueRouter.install.installed = true
 
     // 2. 把 Vue 的构造函数记录在全局
-    _Vue = Vue;
+    _Vue = Vue
 
     // 3. 把创建 Vue 的实例传入的 router 对象注入到 Vue 实例
-
     _Vue.mixin({
-      beforeCreate() {
+      beforeCreate () {
         if (this.$options.router) {
-          _Vue.prototype.$router = this.$options.router;
+          _Vue.prototype.$router = this.$options.router
         }
       }
-    });
+    })
   }
 
-  constructor(options) {
-    this.options = options;
-    this.routeMap = {};
+  constructor (options) {
+    this.options = options
+    this.routeMap = {}
     // observable
     this.data = _Vue.observable({
-      current: "/"
-    });
+      current: '/'
+    })
 
-    this.init();
+    this.init()
   }
 
-  init() {
-    this.createRouteMap();
-    this.initComponent(_Vue);
-    this.initEvent();
+  init () {
+    this.createRouteMap()
+    this.initComponent(_Vue)
+    this.initEvent()
   }
 
-  createRouteMap() {
+  createRouteMap () {
     // 遍历所有的路由规则，把路由规则解析成键值对的形式存储到 routeMap 中
     this.options.routes.forEach(route => {
-      this.routeMap[route.path] = route.component;
-    });
+      this.routeMap[route.path] = route.component
+    })
   }
 
-  initComponent(Vue) {
-    Vue.component("router-link", {
+  initComponent (Vue) {
+    const self = this
+    Vue.component('router-link', {
       props: {
         to: String
       },
-      render(h) {
-        return h(
-          "a",
-          {
-            attrs: {
-              href: this.to
-            },
-            on: {
-              click: this.clickhander
-            }
+      render (h) {
+        return h('a', {
+          attrs: {
+            href: self.options.mode === 'hash' ? '#' + this.to : this.to
           },
-          [this.$slots.default]
-        );
+          on: {
+            click: this.clickhander
+          }
+        }, [this.$slots.default])
       },
       methods: {
-        clickhander(e) {
-          history.pushState({}, "", this.to);
-          this.$router.data.current = this.to;
-          e.preventDefault();
+        clickhander (e) {
+          if (self.options.mode === 'hash') {
+            location.hash = this.to
+          } else {
+            history.pushState({}, '', this.to)
+          }
+
+          this.$router.data.current = this.to
+          e.preventDefault()
         }
       }
-      // template: "<a :href='to'><slot></slot><>"
-    });
-    const self = this;
-    Vue.component("router-view", {
-      render(h) {
-        const cm = self.routeMap[self.data.current];
-        return h(cm);
+      // template: "<a :href='to'><slot></slot></a>"
+    })
+    Vue.component('router-view', {
+      render (h) {
+        const cm = self.routeMap[self.data.current]
+        return h(cm)
       }
-    });
+    })
   }
-  initEvent() {
-    window.addEventListener("popstate", () => {
-      this.data.current = window.location.pathname;
-    });
+
+  initEvent () {
+    window.addEventListener('popstate', () => {
+      this.data.current = window.location.pathname
+    })
+
+    window.addEventListener('hashchange', () => {
+      this.data.current = window.location.hash.slice(1)
+    })
   }
 }
